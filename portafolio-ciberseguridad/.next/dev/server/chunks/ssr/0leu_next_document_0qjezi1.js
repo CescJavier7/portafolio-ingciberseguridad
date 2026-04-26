1b513836db6389eccc76f973976fecaede19a258
CescJavier7 <montatixe@MacBook-Air-de-Javier.local>
2026-04-25 20:56:37 -0500

    fix: corregir repositorio anidado y subir archivos

---
+++ portafolio-ciberseguridad/.next/dev/server/chunks/ssr/0leu_next_document_0qjezi1.js
@@ -0,0 +1,8 @@
+module.exports = [
+"[project]/Documents/CescJavier7/portafolio-ciberseguridad/node_modules/next/document.js [ssr] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+const e = new Error("Could not parse module '[project]/Documents/CescJavier7/portafolio-ciberseguridad/node_modules/next/document.js', file not found");
+e.code = 'MODULE_UNPARSABLE';
+throw e;
+}),
+];
