f6022ad861df2134cb045741d1987821b3ffcbac
CescJavier7 <montatixe@MacBook-Air-de-Javier.local>
2026-04-25 21:07:05 -0500

    feat: estructura raiz limpia

--- portafolio-ciberseguridad/.next/dev/server/pages/_document.js
+++
@@ -1,4 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
-R.c("server/chunks/ssr/0leu_next_document_0qjezi1.js")
-R.m("[project]/Documents/CescJavier7/portafolio-ciberseguridad/node_modules/next/document.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/Documents/CescJavier7/portafolio-ciberseguridad/node_modules/next/document.js [ssr] (ecmascript)").exports
